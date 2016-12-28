556789766cd0c2444053645befdb9a1a8715073b
Alyson Stiffel <alysonstiffel@gmail.com>
2016-12-28 14:07:12 +0000

    image blur

--- image2.rb
+++ image2.rb
@@ -6,9 +6,30 @@ class Image
 
   def output_image
     @image.each do |row|
-      puts row.join
+      row.each do |pixel|
+        print pixel
+    end
+    puts
+  end
+
+  # build new img and return blurred img
+  def transform
+    # loop through all cells
+    one_pixels = []
+    @image.each_with_index do |row, row_index|
+      row.each_with_index do |pixel, col_index|
+        if pixel == 1
+          # do more stuff
+        else
+          # do more stuff
+        end
     end
   end
+    return Image.new(@image)
+  end
+
+  # loop through, find ones and store
+  # then go to ones and blur
 
 end
 
@@ -18,4 +39,22 @@ image = Image.new([
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
-image.output_image
+# image.transform
+# image.output_image
+
+afterImage = image.transform
+afterImage.output_image
+
+afterImage = Image.new([
+  [0, 1, 0, 0],
+  [1, 1, 1, 1],
+  [0, 1, 1, 1],
+  [0, 0, 0, 1]
+])
+
+# afterImage = Image.new([
+#   [0, 1, 0, 0],
+#   [1, 1, 1, 1],
+#   [0, 1, 1, 1],
+#   [0, 0, 0, 1]
+# ])
