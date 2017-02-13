029c24248ad9b0385091a9d61260ca3dac9a4355
Alyson Stiffel <alysonstiffel@gmail.com>
2017-02-13 23:15:38 +0000

    linkedlist

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,3 +1,22 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+
+  def print_values(list_node)
+      if list_node
+        print "#{list_node.value} --> "
+        print_values(list_node.next_node)
+      else
+        print "nil\n"
+        return
+      end
+  end
+end
+
 class Stack
     attr_reader :data
 
@@ -7,17 +26,16 @@ class Stack
 
     # Push a value onto the stack
     def push(value)
-        # IMPLEMENT ME!
-        print "#{value} -->"
+        @data = Stack.new(value, @data)
         stack.push(1)
     end
 
     # Pop an item off the stack.
-    # Remove the last item that was pushed onto the
-    # stack and return the value to the user
+    # Remove the last item that was pushed
+    # and return the value to the user
     def pop
-        # I RETURN A VALUE
         puts stack.pop
+        # print "#{value} -->"
     end
 
     def reverse_list(list)
@@ -30,8 +48,11 @@ class Stack
 
         # ADD CODE HERE
     end
-
 end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
 
 print_values(node3)
 
