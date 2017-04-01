f15ea34efc39324b9c07adc123c08af46dea9b73
Alyson Stiffel <alysonstiffel@gmail.com>
2017-04-01 12:57:21 -0400

    luhn

--- luhn.rb
+++ luhn.rb
@@ -1,29 +1,35 @@
 module Luhn
-  # why module, not class?
-  # initialize v. new?
 
   def self.is_valid?(number)
-    self.new.is_valid?(number)
-  end
-
-  def is_valid?(number)
     sum = 0
     number_array = number.to_s.split('') # break cc into ind. digits
     number_array = number_array.reverse # start from the right
 
-    # loop through every digit_with_index and add the digit to the sum
+    # loop through every digit_with_index
+    # add the digit to the sum
     number_array.each_with_index do |digit, index|
-    if (index % 2 != 0) # every other number starting with the first
-      sum += digit.to_i * 2 >= 10 ? digit.to_i*2-9 : digit.to_i*2
-    else
-      sum += digit.to_i
+      d = digit.to_i
+
+      # every other number starting with the first
+      if index.odd? 
+        sum += odd_value_sum(d)
+      else
+        sum += d
+      end
     end
-  end
 
     if (sum % 10) == 0
       return true
     else
       return false
     end
+  end
+
+  # Luhn algo explanation, etc.
+  def self.odd_value_sum(digit)
+    doubled = digit * 2
+
+    return doubled if doubled < 10
+    doubled - 9 
   end
 end
--- luhn_notes.rb
+++ luhn_notes.rb
@@ -1,3 +1,33 @@
+module Luhn
+  # why module, not class?
+  # initialize v. new?
+
+  def self.is_valid?(number)
+    self.new.is_valid?(number)
+  end
+
+  def is_valid?(number)
+    sum = 0
+    number_array = number.to_s.split('') # break cc into ind. digits
+    number_array = number_array.reverse # start from the right
+
+    # loop through every digit_with_index and add the digit to the sum
+    number_array.each_with_index do |digit, index|
+    if (index % 2 != 0) # every other number starting with the first
+      sum += digit.to_i * 2 >= 10 ? digit.to_i*2-9 : digit.to_i*2
+    else
+      sum += digit.to_i
+    end
+  end
+
+    if (sum % 10) == 0
+      return true
+    else
+      return false
+    end
+  end
+end
+
 # step 1
 valid.to_s
 valid.to_s.split('')
