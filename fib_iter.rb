53af19643f222d3fd8f2804833237277037e36ff
Alyson Stiffel <alysonstiffel@gmail.com>
2017-04-09 09:27:35 -0400

    fib

--- fib_iter.rb
+++ fib_iter.rb
@@ -1,1 +1,8 @@
 # fibonacci iterative
+
+require 'benchmark'
+num = 35
+Benchmark.bm do |x|
+  x.report("recursive_fib") { recursive_fib(num) }
+  x.report("iterative_fib")  { iterative_fib(num)  }
+end
--- fib_rec.rb
+++ fib_rec.rb
@@ -1,1 +1,13 @@
 # fibonacci recursive
+
+def fib(n)
+  return n if n <= 1
+  fib(n-1) + fib(n-2)
+end
+
+require 'benchmark'
+num = 35
+Benchmark.bm do |x|
+  x.report("recursive_fib") { recursive_fib(num) }
+  x.report("iterative_fib")  { iterative_fib(num)  }
+end
