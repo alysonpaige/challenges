82d9d9efe63bbc6bee294801299480f6883619fb
Alyson Stiffel <alysonstiffel@gmail.com>
2017-04-10 13:39:00 -0400

    fib

--- fib_iter.rb
+++ fib_iter.rb
@@ -1,26 +1,20 @@
 # fibonacci iterative
 
 def fib(num)
-  x = 0
-  y = 1
+  fib0 = 0
+  fib1 = 1
   num.times do
-    temp_num = x
-    x = y
-    y = temp_num + y
+    fib0, fib1 = fib1, fib0 + fib1
   end
-    return x
+  return fib0
 end
 
-35.times do |num|
-  output = fib(num)
-  puts output
-end
-
+puts fib(35)
 # puts "#{num}'s fibonacci value is #{fib(num)}"
 
-require 'benchmark'
-num = 35
-Benchmark.bm do |x|
-  x.report("recursive_fib") { recursive_fib(num) }
-  x.report("iterative_fib")  { iterative_fib(num)  }
-end
+# require 'benchmark'
+# num = 35
+# Benchmark.bm do |x|
+#   x.report("recursive_fib") { recursive_fib(num) }
+#   x.report("iterative_fib")  { iterative_fib(num)  }
+# end
--- fib_rec.rb
+++ fib_rec.rb
@@ -1,21 +1,16 @@
 # fibonacci recursive
 
-# input n
-# if n = 1, output 1
-# if n > 2, output sum of previous two values
-# print the output of n
-
-num = 35
 def fib(num)
   return num if num <= 1
   fib(num-1) + fib(num-2)
 end
 
-puts "#{num}'s fibonacci value is #{fib(num)}"
+puts fib(35)
+# puts "#{num}'s fibonacci value is #{fib(num)}"
 
-require 'benchmark'
-num = 35
-Benchmark.bm do |x|
-  x.report("recursive_fib") { recursive_fib(num) }
-  x.report("iterative_fib")  { iterative_fib(num)  }
-end
+# require 'benchmark'
+# num = 35
+# Benchmark.bm do |x|
+#   x.report("recursive_fib") { recursive_fib(num) }
+#   x.report("iterative_fib")  { iterative_fib(num)  }
+# end
