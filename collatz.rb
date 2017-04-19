d65e0ae2d0faa0afadb29c0b94fa592776ffd4ff
Alyson Stiffel <alysonstiffel@gmail.com>
2017-04-19 11:29:52 -0400

    collatz

--- collatz.rb
+++ collatz.rb
@@ -1,23 +1,22 @@
-# class Collatz
-  def sequence(n, counter = 0)
-    counter += 1
-    return counter if n == 1
-    if n.even?
-      sequence(n / 2, counter)
-    else
-      sequence(3 * n + 1, counter)
+def length_of_collatz_sequence(n, counter = 0)
+  counter += 1
+  return counter if n == 1
+  if n.even?
+    length_of_collatz_sequence(n / 2, counter)
+  else
+    length_of_collatz_sequence(3 * n + 1, counter)
+  end
+end
+
+def longest_collatz_seq(n)
+  longest_sequence = 0
+  (1..n).each do |n|
+    seq_length = length_of_collatz_sequence(n)
+    if seq_length > longest_sequence
+      longest_sequence = seq_length
     end
   end
+  longest_sequence
+end
 
-  def length
-    longest_sequence = 0
-    (1..n).each do |n|
-      seq_length = sequence(n)
-      if seq_length > longest_sequence
-        longest_sequence = seq_length
-      end
-    end
-  end
-# end
-
-puts sequence(7)
+puts longest_collatz_seq(1_000_000)
