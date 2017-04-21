64c84ce5dfc9d57196f166746c24dfd70a2417af
Alyson Stiffel <alysonstiffel@gmail.com>
2017-04-20 20:29:26 -0400

    collatz

--- collatz.rb
+++ collatz.rb
@@ -1,29 +1,29 @@
-def length_of_collatz_sequence(n, counter = 0)
+def length_of_collatz_sequence(i, counter = 0)
   counter += 1
-  return counter if n == 1
-  if n.even?
-    length_of_collatz_sequence(n / 2, counter)
+  return counter if i == 1
+  if i.even?
+    length_of_collatz_sequence(i / 2, counter)
   else
-    length_of_collatz_sequence(3 * n + 1, counter)
+    length_of_collatz_sequence(3 * i + 1, counter)
   end
 end
 
 def longest_collatz_seq(n)
   longest_sequence_length = 0
-  (1..n).each do |n|
-    seq_length = length_of_collatz_sequence(n)
+  (1..n).each do |i|
+    seq_length = length_of_collatz_sequence(i)
     if seq_length > longest_sequence_length
       longest_sequence_length = seq_length
     end
   end
-  longest_sequence_length
+  # longest_sequence_length
 end
 
-puts longest_collatz_seq(1_000_000)
+puts longest_collatz_seq(6)
+puts "Input #{n} has a sequence length of #{i}."
 
 # n = 1_000_000
 # longest_sequence_length = 525
-# input "n" has sequence length of 525
 
 # require 'benchmark'
 # n = 1_000_000
