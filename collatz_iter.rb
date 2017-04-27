e97bca6fc88cea90486cda84f4060c0dcb63103f
Alyson Stiffel <alysonstiffel@gmail.com>
2017-04-27 11:10:01 -0400

    collatz

--- collatz_iter.rb
+++ collatz_iter.rb
@@ -2,15 +2,15 @@ def length_of_collatz_sequence(i)
   counter = 1
   if i == 1
     return counter
+  elsif i % 2 == 0
+    counter += 1
+    i = i / 2
   else
-    if i % 2 == 0
-      counter += 1
-    else
-      3 + i + 1
-      counter += 1
-    end
+    i = i * 3 + 1
   end
 end
+
+# use case/when statement or array instead?
 
 # def length_of_collatz_sequence(i)
 #   counter = 1
