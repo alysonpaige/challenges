13c49f02c1355afac383515037736a766b173ccb
Alyson Stiffel <alysonstiffel@gmail.com>
2017-04-27 09:50:09 -0400

    collatz

--- collatz_iter.rb
+++ collatz_iter.rb
@@ -1,19 +1,23 @@
 def length_of_collatz_sequence(i)
-  stack = []
-  while i 
-end
-
-def longest_collatz_seq(n)
-  input_that_produces_the_longest_sequence_length = 0
-  longest_sequence_length = 0
-  (1..n).each do |i|
-    seq_length = length_of_collatz_sequence(i)
-    if seq_length > longest_sequence_length
-      longest_sequence_length = seq_length
-      input_that_produces_the_longest_sequence_length = i
-    end
+  counter = 1
+  while true
+    return counter if i == 1
+    i = i % 2 == 0 ? i / 2 : 3 * i + 1
+    counter += 1
   end
-  input_that_produces_the_longest_sequence_length
 end
 
-puts longest_collatz_seq(6)
+# def length_of_collatz_sequence(i, counter = 0)
+#   counter += 1
+#   return counter if i == 1
+#   stack = []
+#   if i != 1
+#     stack.push(i / 2, counter)
+#   else
+#     stack.push(3 * i + 1, counter)
+#   end
+#   stack
+# end
+
+puts length_of_collatz_sequence(6)
+# => 9
