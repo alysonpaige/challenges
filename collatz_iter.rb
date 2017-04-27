e39006f5bfc2f74de8a0c5214153f542c126df6d
Alyson Stiffel <alysonstiffel@gmail.com>
2017-04-27 10:23:12 -0400

    collatz

--- collatz_iter.rb
+++ collatz_iter.rb
@@ -1,22 +1,24 @@
 def length_of_collatz_sequence(i)
   counter = 1
-  while true
-    return counter if i == 1
-    i = i % 2 == 0 ? i / 2 : 3 * i + 1
-    counter += 1
+  if i == 1
+    return counter
+  else
+    if i % 2 == 0
+      counter += 1
+    else
+      3 + i + 1
+      counter += 1
+    end
   end
 end
 
-# def length_of_collatz_sequence(i, counter = 0)
-#   counter += 1
-#   return counter if i == 1
-#   stack = []
-#   if i != 1
-#     stack.push(i / 2, counter)
-#   else
-#     stack.push(3 * i + 1, counter)
+# def length_of_collatz_sequence(i)
+#   counter = 1
+#   while true
+#     return counter if i == 1
+#     i = i % 2 == 0 ? i / 2 : 3 * i + 1
+#     counter += 1
 #   end
-#   stack
 # end
 
 puts length_of_collatz_sequence(6)
