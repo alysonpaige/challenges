7210b752762c7d0af7cd566cb143ea1a4f0959fd
Alyson Stiffel <alysonstiffel@gmail.com>
2017-05-04 14:33:56 -0400

    tree

---
+++ tree.rb
@@ -0,0 +1,31 @@
+class Tree
+  attr_accessor :payload, :children
+
+  def initialize(payload, children)
+    @payload = payload
+    @children = children
+  end
+
+  def dfs(node, value)
+    return node if node.payload == value
+    node.children.each do |child|
+      found = dfs(child, value)
+      return found if found
+    end
+  end
+
+end
+
+# The "Leafs" of a tree, elements that have no children
+deep_fifth_node = Tree.new(5, [])
+eleventh_node = Tree.new(11, [])
+fourth_node   = Tree.new(4, [])
+
+# The "Branches" of the tree
+ninth_node = Tree.new(9, [fourth_node])
+sixth_node = Tree.new(6, [deep_fifth_node, eleventh_node])
+seventh_node = Tree.new(7, [sixth_node])
+shallow_fifth_node = Tree.new(5, [ninth_node])
+
+# The "Trunk" of the tree
+trunk = Tree.new(2, [seventh_node, shallow_fifth_node])
