6bece3e8e654d63fe6df6e6122e3e9c748e5c71a
Alyson Stiffel <alysonstiffel@gmail.com>
2017-05-10 14:09:29 -0400

    tree

--- btree.rb
+++ btree.rb
@@ -9,28 +9,50 @@ class BinaryTree
 
 end
 
-class BTreeSort
+class TreeBuilder
 
-  def initialize
-    @treesort = []
+  def initialize(input_array)
+    @tree = BinaryTree.new(input_array[0], nil, nil)
+    input_array.shift
+    @array = input_array
   end
 
-  def self.sort(array)
-    tree_representation = build_tree(array)
-    return flatten_tree(tree_representation)
+  def build
+    array.each do |value|
+      insert_to_tree(value, @tree)
+    end
   end
 
-  def flatten_tree(tree)
-    # build new array, flattening the tree
-  end
-
-  def build_tree(array)
-    first = array[0]
-    tree = BinaryTree.new(first, nil, nil)
-    rest = array.last(array.length - 1)
+  def insert_to_tree(value, curr_node)
+    if value < curr_node.payload && curr_node.left == nil
+      curr_node.left = BinaryTree.new(value, nil, nil)
+    elsif value > curr_node.payload && curr_node.right == nil
+      curr_node.right = BinaryTree.new(value, nil, nil)
+    elsif value < curr_node.payload
+      insert_to_tree(value, curr_node.right)
+    end
   end
 
 end
+
+  # def initialize
+  #   @treesort = []
+  # end
+
+  # def self.sort(array)
+  #   tree_representation = build_tree(array)
+  #   return flatten_tree(tree_representation)
+  # end
+
+  # def flatten_tree(tree)
+  #   # build new array, flattening the tree
+  # end
+
+  # def build_tree(array)
+  #   first = array[0]
+  #   tree = BinaryTree.new(first, nil, nil)
+  #   rest = array.last(array.length - 1)
+  # end
 
 seven = BinaryTree.new(7, nil, nil)
 five  = BinaryTree.new(5, nil, nil)
@@ -40,4 +62,5 @@ three = BinaryTree.new(3, nil, six)
 two   = BinaryTree.new(2, four, five)
 trunk = BinaryTree.new(1, two, three)
 
-array = [7, 5, 4, 6, 3, 2]
+# expected = [1, 4, 6, 7, 9, 10, 14]
+# array = [7, 4, 9, 1, 6, 14, 10]
