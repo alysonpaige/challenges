73a8263358fe4a9ca46482123d354f7d6feff5e9
Alyson Stiffel <alysonstiffel@gmail.com>
2017-06-05 13:20:09 -0400

    prototypical inheritance

--- js/proto_inheritance.js
+++ js/proto_inheritance.js
@@ -1,10 +1,10 @@
 function Card(rank, suit) {
-  this.rank = rank;
-  this.suit = suit;
+    this.rank = rank;
+    this.suit = suit;
 }
 
 Card.prototype.outputCard = function(){
-  console.log(this.rank + " of " + this.suit);
+    console.log(this.rank + " of " + this.suit);
 }
 
 var fourOfDiamonds = new Card(4, "diamonds");
