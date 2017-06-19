4f97807a72bff28514018ea425aae3411cd29726
Alyson Stiffel <alysonstiffel@gmail.com>
2017-06-19 13:19:20 -0400

    map

--- js/map.js
+++ js/map.js
@@ -1,16 +1,17 @@
 "use strict";
 
 var _ = {
-  map: (array, callback) => {
-    return 'FAIL :(';
+  map(input) {
+    return input.map(function(input) {
+      return input.multiplyBy10;
+    });
   }
-}
+};
 
 // Define a method that multiplies the input by 10
 function multiplyBy10(value) {
   return 10 * value;
 }
-
 const value = _.map([1, 2, 3], multiplyBy10);
 
 // value => [10, 20, 30]
