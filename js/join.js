31a351cc378cda9ede0f25b519a9ef6d5184a1e4
Alyson Stiffel <alysonstiffel@gmail.com>
2017-06-27 20:57:40 -0400

    join

--- js/join.js
+++ js/join.js
@@ -8,7 +8,7 @@ var _ = {
       for (i=0; i < value.length; i++) {
         result = result + value[i] + sep;
       }
-        return result;
+      return result;
     }
 };
 
@@ -16,3 +16,22 @@ const joined = _.join(["hello", "goodbye"], ", ")
 const output = joined.replace(/,\s*$/, "");
 
 console.log(output);
+
+// alt to removing extra comma from output
+var _ = {
+    join: function(value) {
+      var result = "";
+      var sep = ", ";
+      var i;
+      for (i=0; i < value.length-1; i++) {
+        result = result + value[i] + sep;
+      }
+        result = result + value[value.length-1]
+        return result;
+    }
+};
+
+const joined = _.join(["hello", "goodbye"], ", ")
+// const output = joined.replace(/,\s*$/, "");
+
+console.log(joined);
