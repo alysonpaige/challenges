1c1e091cec77fc0d6f26e019c194cf3384a71fe1
Alyson Stiffel <alysonstiffel@gmail.com>
2017-06-27 20:59:56 -0400

    map

--- js/map.js
+++ js/map.js
@@ -16,3 +16,28 @@ const value = _.map([1, 2, 3], multiplyBy10);
 
 // value => [10, 20, 30]
 console.log(value);
+
+
+// alt solution:
+var object = {
+  nap(input, fn) {
+    var result = [];
+    // loop over all values of input
+    for (var i = 0; i < input.length; i++) {
+      var x = input[i];
+      var xFn = fn(x);
+      result.push(xFn);
+    }
+    return result
+  }
+};
+
+// Define a method that multiplies the input by 10
+function multiplyBy10(value) {
+  return 10 * value;
+}
+
+const value = object.nap([1, 2, 3], multiplyBy10);
+
+// value => [10, 20, 30]
+console.log(value);
