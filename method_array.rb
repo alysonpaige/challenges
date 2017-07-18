a4b9ceeeea2441f0b5dbd6a86efb0cfea2465bc3
Alyson Stiffel <alysonstiffel@gmail.com>
2017-07-18 14:30:13 -0400

    rejct block

--- method_array.rb
+++ method_array.rb
@@ -5,11 +5,11 @@ end
 ray = [1,1,2,2,7,8,9,9,9,4,2,2,2,1,1,3,2,20,40]
 method(ray)
 
+# the method "method" is defined with a parameter of "array"
+# it calls a block to print a new array after
+# counting the defined variable "element" within the array
+# and if the "element" is listed more than once, remove it
+# then it assigns an array to "ray"
+# calls the method with the argument of "ray"
+# for an output of:
 # => [7, 8, 4, 3, 20, 40]
-
-# the method "method" is defined with an "array" argument
-# it calls a block to print a new array
-# counting the defined "elements" in the array
-# and if they're listed more than once, remove them
-# assigns an array value to the key "ray"
-# calls the method with the parameter of "ray"
