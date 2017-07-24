b4cf9fcdab8384d715ea166d5d28683099dde83f
Alyson Stiffel <alysonstiffel@gmail.com>
2017-07-24 12:20:06 -0400

    method hex

--- method_hex.rb
+++ method_hex.rb
@@ -7,5 +7,12 @@ end
 
 p method(0x33BAE7)
 
-
+# the method "method" is defined with a parameter of "hex_value"
+# `hex_pairs` is assigned to make `hex_value` a hexadecimal string with a base of 16
+# made to be in upcase and scanned for a regular expression /../
+# map is then run on `hex_pairs` which runs the block for each element
+# and outputs each returned value from the block e.g. [x, y, z]
+# the value `x` is divided by 255.0 and rounded to 2 decimal points e.g. 0.36 vs. 0.1
+# calls the method to be printed with the argument of "0x33BAE7"
+# for an output of:
 # => [0.2, 0.73, 0.91]
