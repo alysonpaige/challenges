eb373d0eb108c3df63f80f3c525b6585b0f12c65
Alyson Stiffel <alysonstiffel@gmail.com>
2017-08-02 13:44:12 -0400

    adjacent alt

--- adjacent.rb
+++ adjacent.rb
@@ -7,3 +7,14 @@ def adjacentElementsProduct(inputArray)
 end
 
 p adjacentElementsProduct([3, 6, -2, -5, 7, 3])
+
+
+
+# alt solution
+def adjacentElementsProduct(inputArray)
+    productArray = []
+    (inputArray.length - 1).times do |i|
+        productArray << inputArray[i] * inputArray[i + 1]
+    end
+    productArray.max
+end
