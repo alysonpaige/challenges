2bcbc631117cb4f5ff020a079f36db4f90583fd4
Alyson Stiffel <alysonstiffel@gmail.com>
2017-08-04 14:29:12 -0400

    consec array

---
+++ consec_array.rb
@@ -0,0 +1,24 @@
+# make array consecutive
+
+def makeArrayConsecutive2(statues)
+    if statues.length == 1
+        return 0
+    else
+        return (statues.length-1)
+    # statues.sort_by(&:to_i)
+    end
+end
+
+p makeArrayConsecutive2([6, 2, 3, 8])
+# => 3
+
+p makeArrayConsecutive2([0, 3])
+# => 2
+
+p makeArrayConsecutive2([5, 4, 6])
+# => 0
+
+p makeArrayConsecutive2([6, 3])
+# => 2
+
+puts "should be 3, 2, 0, 2"
