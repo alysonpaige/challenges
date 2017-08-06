28df7d2b14d795602806a6cc18837f0a3e6b3f5d
Alyson Stiffel <alysonstiffel@gmail.com>
2017-08-05 22:03:28 -0400

    array length

--- consec_array.rb
+++ consec_array.rb
@@ -1,12 +1,20 @@
 # make array consecutive
 
 def makeArrayConsecutive2(statues)
-    if statues.length == 1
-        return 0
-    else
-        return (statues.length-1)
-    # statues.sort_by(&:to_i)
-    end
+  if statues.length == 1
+    return 0
+  else
+    return (statues.length-1)
+  end
+  # count = 0
+  # if statues.length == 1
+  #   return 0
+  # elsif statues.length-1
+  #   return statues
+  # else count += 1
+  #   return count
+  # # statues.sort_by(&:to_i)
+  # end
 end
 
 p makeArrayConsecutive2([6, 2, 3, 8])
