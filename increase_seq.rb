1a70a7129c6c008d1645e7dbcaed0131f65ad810
Alyson Stiffel <alysonstiffel@gmail.com>
2017-08-08 09:21:19 -0400

    increasing sequence

---
+++ increase_seq.rb
@@ -0,0 +1,10 @@
+# Given a sequence of integers as an array,
+# determine whether it is possible to obtain a strictly increasing sequence
+# by removing no more than one element from the array.
+
+def almostIncreasingSequence(sequence)
+
+end
+
+# [1, 10, 7, 9, 8] => false
+# [1, 10, 7, 9, 10] => true
