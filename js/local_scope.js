ab73127cc018fa11e580907a383b191266655847
Alyson Stiffel <alysonstiffel@gmail.com>
2017-11-11 21:02:40 -0500

    local variable

---
+++ js/local_scope.js
@@ -0,0 +1,11 @@
+// override the global variable so the value of `outerWear` is "sweater"
+
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  var outerWear = "sweater";
+  
+  return outerWear;
+}
+
+myOutfit();
