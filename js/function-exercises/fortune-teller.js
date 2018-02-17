56047a662911baebed8cfa798c6d05c8f5783aad
Alyson Stiffel <alysonstiffel@gmail.com>
2018-02-17 11:25:06 -0500

    fortune function

---
+++ js/function-exercises/fortune-teller.js
@@ -0,0 +1,5 @@
+function tellFortune(children, partner, location, job) {
+  return ("You will be a " + job + " in " + location + ", and married to " + partner + " with " + children + " kids.");
+}
+
+console.log(tellFortune(2, "Brian", "Panama", "programmer"));
