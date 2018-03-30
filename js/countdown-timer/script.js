631b21bcf358a6322a5a1f59066e048e3234184e
Alyson Paige <alysonstiffel@gmail.com>
2018-03-30 08:26:54 -0400

    function name updates

--- js/countdown-timer/script.js
+++ js/countdown-timer/script.js
@@ -1,12 +1,17 @@
-// countdown timer
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+function countdownCalc() {
+  // set date to count down to
+  var countdownDate = new Date("June 14, 2018 11:00:00").getTime();
+  // todays date and time
+  var now = new Date().getTime();
+  // find distance between now and the coutdown date
+  var distance = countdownDate - now;
+  // time calculations for days, hours, minutes and seconds
+  var seconds = Math.floor((distance / 1000) % 60);
+  var minutes = Math.floor((distance / 1000 / 60) % 60);
+  var hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   return {
-    'total': t,
+    'total': distance,
     'days': days,
     'hours': hours,
     'minutes': minutes,
@@ -14,31 +19,32 @@ function getTimeRemaining(endtime) {
   };
 }
 
+// initialize clock
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
   var daysSpan = clock.querySelector('.days');
   var hoursSpan = clock.querySelector('.hours');
   var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
-
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
+  // display result
+  function displayClock() {
+    var time = countdownCalc(endtime);
+  
+    daysSpan.innerHTML = time.days;
+    hoursSpan.innerHTML = ('0' + time.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + time.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + time.seconds).slice(-2);
+  
+    // When countdown is finished
+    if (time.total <= 0) {
       clearInterval(timeinterval);
+      
+      // console.log('Kick off!');
     }
   }
-
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
+  displayClock();
+  var timeinterval = setInterval(displayClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var deadline = new Date("June 14, 2018 11:00:00").getTime();
 initializeClock('clockdiv', deadline);
-
-// new Date("June 14, 2018 11:00:00").getTime()
