3e37ae938b67e1ae6b297cd25fecbca7ddcaed28
Alyson Paige <alysonstiffel@gmail.com>
2018-06-14 14:59:24 -0400

    multiply by length

---
+++ js/times-length.js
@@ -0,0 +1,9 @@
+/*
+Create a function to multiply all values in an array by the amount of values in that array
+* - Example(2, 3, 1, 0 becomes 8,12,4,0)
+* - NOTE: All values are numbers.
+*/
+
+function MultiplyByLength(arr) {
+
+}
