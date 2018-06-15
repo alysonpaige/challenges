56eca25366b537ea3cb915562cb6ead711cdd08b
Alyson Paige <alysonstiffel@gmail.com>
2018-06-15 08:48:12 -0400

    find index

---
+++ js/find-index.js
@@ -0,0 +1,8 @@
+/*
+Create a function that takes an array and a string as arguments and return the index of the string.
+*/
+
+function find_index(lst, str) {
+  const arrIndex = lst.indexOf(str);
+  return arrIndex;
+}
