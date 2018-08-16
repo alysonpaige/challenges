a6be852e4b17bb5fe9684560c39de1fb1e708990
Alyson Paige <alysonstiffel@gmail.com>
2018-08-15 21:11:00 -0400

    remove vowels regex

--- js/remove-vowels.js
+++ js/remove-vowels.js
@@ -3,5 +3,6 @@ Create a function that takes a string and returns a new string with all vowels r
 */
 
 function silenceVowels(str) {
-	
+  var regex = /[aeiou]/ig;
+  return str.replace(regex, '');
 }
