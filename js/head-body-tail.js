1556aa117a150e66ed7a3cc22d7c05eaa7f30e60
Alyson Paige <alysonstiffel@gmail.com>
2018-08-20 14:33:29 -0400

    string argument challenge

---
+++ js/head-body-tail.js
@@ -0,0 +1,7 @@
+/*
+Write a function that takes four string arguments. You will be comparing the first string to the three next strings. Verify if the first string starts with the second string, includes the third string, and ends with the fourth string. If the first string passes all checks, return the string "My head, body, and tail.", otherwise, return "Incomplete.".
+*/
+
+function verifySubstrs(mainStr, head, body, tail) {
+	
+}
