04a569a6d0bfa747c75e5b6605baa8469b3fe1cb
Alyson Paige <alysonstiffel@gmail.com>
2018-08-21 08:42:26 -0400

    verify substr solution

--- js/head-body-tail.js
+++ js/head-body-tail.js
@@ -3,5 +3,17 @@ Write a function that takes four string arguments. You will be comparing the fir
 */
 
 function verifySubstrs(mainStr, head, body, tail) {
-	
-}
+  let str1 = body.split('');
+  str1.shift();
+  str1 = str1.join('');
+  
+  let str2 = tail.split('');
+  str2.shift();
+  str2 = str2.join('');
+
+  if ((mainStr === head + body + tail) || (mainStr === head + str1 + str2)) {
+    return 'My head, body, and tail.';
+  } else {
+    return 'Incomplete.';
+  }
+};
