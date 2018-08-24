2610a9c8d8e729e59daa4bf88a154a73518469d0
Alyson Paige <alysonstiffel@gmail.com>
2018-08-24 13:48:11 -0400

    last item alt solution

--- js/last-item.js
+++ js/last-item.js
@@ -5,3 +5,8 @@ Create a function that accepts an array and returns the last item in the array.
 function getLastItem(arr) {
 	return arr.slice(-1).pop();
 }
+
+// alt solution
+function getLastItem(arr) {
+  return arr[arr.length - 1];
+}
